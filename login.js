64d10c51bcccb6b5b251f096033ba7eb221260d8
CodeTestGroup <147273420+CodeTestGroup@users.noreply.github.com>
2023-10-08 08:44:59 +0100

    Uploaded web

--- login.js
+++ login.js
@@ -4,7 +4,8 @@ function validate(){
 var username = document.getElementById("username").value;
 var password = document.getElementById("password").value;
 if ( username == "ctadmin" && password == "katy"){
-window.location.href = 'downloads/';
+showdownloads();
+hidelogin();
 return false;
 }
 else{
